40f7ecfa4b761172b8783949e89939c96fdbf80f
Nitish Bharadwaj <nitish@emproto.com>
2021-03-23 17:24:46 +0530

    Removed href in navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand">
           Total Items{" "}
           <span className="badge badge-pill badge-secondary">
             {this.props.totalcounters}
